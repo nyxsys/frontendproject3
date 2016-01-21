8b079468d231180765564b5d550ee6edbc3bd1f4
jaaimino <jacobaimino@gmail.com>
2016-01-21 16:47:39 +0000

    Styles loaded from json config now

--- js/switcher.js
+++ js/switcher.js
@@ -1,39 +1,33 @@
-var style1 = [
-    ['p', 'rounded'],
-    ['p', 'thick-border'],
-    ['aside', 'column-1-3'],
-    ['#zen-intro', 'column-2-3'],
-    ['#zen-supporting', 'column-3-3'],
-    ['.page-wrapper', 'relative']
-];
-var style2 = [
-    ['aside', 'column-3-3'],
-    ['#zen-intro', 'column-2-3'],
-    ['#zen-supporting', 'column-1-3'],
-    ['.page-wrapper', 'relative']
-];
-var styles = [style1, style2];
+//Eek a global variable
 var currentStyleIndex = -1;
 
-var removeStylePair = function(aPair) {
-    Array.prototype.forEach.call(document.querySelectorAll(aPair[0]), function($el) {
-        $el.classList.remove(aPair[1]);
-    });
-}
-var applyStylePair = function(aPair) {
-    Array.prototype.forEach.call(document.querySelectorAll(aPair[0]), function($el) {
-        $el.classList.add(aPair[1]);
-    });
-}
+loadJSON("config/switcher.json", function(data){
+    var decoded = JSON.parse(data);
+    setupSwitcher(decoded.styles);
+});
 
-var nextStyle = function() {
-    console.log("Test");
-    var previousStyleIndex = currentStyleIndex; //-1 means none
-    currentStyleIndex = (currentStyleIndex + 1) % styles.length;
-    if (previousStyleIndex > -1) {
-        styles[previousStyleIndex].forEach(removeStylePair);
+function setupSwitcher(styles){
+    
+    var removeStylePair = function(aPair) {
+        console.log(aPair);
+        Array.prototype.forEach.call(document.querySelectorAll(aPair[0]), function($el) {
+            $el.classList.remove(aPair[1]);
+        });
     }
-    styles[currentStyleIndex].forEach(applyStylePair);
+    var applyStylePair = function(aPair) {
+        Array.prototype.forEach.call(document.querySelectorAll(aPair[0]), function($el) {
+            $el.classList.add(aPair[1]);
+        });
+    }
+    
+    var nextStyle = function() {
+        var previousStyleIndex = currentStyleIndex; //-1 means none
+        currentStyleIndex = (currentStyleIndex + 1) % styles.length;
+        if (previousStyleIndex > -1) {
+            styles[previousStyleIndex].forEach(removeStylePair);
+        }
+        styles[currentStyleIndex].forEach(applyStylePair);
+    }
+    
+    document.body.addEventListener('click', nextStyle);
 }
-
-document.body.addEventListener('click', nextStyle);
