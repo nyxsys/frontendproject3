1e3718de60962ad59a7d7b46ce8a0f20cac3dd0b
jaaimino <jacobaimino@gmail.com>
2016-01-21 16:18:43 +0000

    Simplified gulp

--- gulpfile.js
+++ gulpfile.js
@@ -9,56 +9,9 @@ var sourcemaps = require('gulp-sourcemaps');
 // Get one .styl file and render
 gulp.task('one', function () {
   gulp.src('./stylus/style.styl')
-    .pipe(stylus({ use: nib()}))
-    .pipe(gulp.dest('./'));
-});
-
-// Options
-// Options compress
-gulp.task('compress', function () {
-  gulp.src('./stylus/compressed.styl')
-    .pipe(stylus({
-      compress: true
-    }))
-    .pipe(gulp.dest('./'));
-});
-
-
-// Set linenos
-gulp.task('linenos', function () {
-  gulp.src('./stylus/linenos.styl')
-    .pipe(stylus({linenos: true}))
-    .pipe(gulp.dest('./'));
-});
-
-// Include css
-// Stylus has an awkward and perplexing 'include css' option
-gulp.task('include-css', function() {
-  gulp.src('./stylus/*.styl')
-    .pipe(stylus({
-      'include css': true
-    }))
-    .pipe(gulp.dest('./'));
-
-});
-
-// Inline sourcemaps
-gulp.task('sourcemaps-inline', function () {
-  gulp.src('./stylus/sourcemaps-inline.styl')
-    .pipe(sourcemaps.init())
-    .pipe(stylus())
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./'));
-});
-
-// External sourcemaps
-gulp.task('sourcemaps-external', function () {
-  gulp.src('./stylus/sourcemaps-external.styl')
-    .pipe(sourcemaps.init())
-    .pipe(stylus())
-    .pipe(sourcemaps.write('.'))
+    .pipe(stylus({ use: nib(), compress: true}))
     .pipe(gulp.dest('./'));
 });
 
 // Default gulp task to run
-gulp.task('default', ['one', 'compress', 'linenos', 'sourcemaps-inline', 'sourcemaps-external']);
+gulp.task('default', ['one']);
