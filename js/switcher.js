9718e371650628d11f0783c4d01089c07176e7a4
jaaimino <jacobaimino@gmail.com>
2016-01-22 16:25:03 +0000

    New random style system

--- js/switcher.js
+++ js/switcher.js
@@ -1,12 +1,16 @@
-//Eek a global variable
-var currentStyleIndex = -1;
+//Eek global variables
+var prevStyles = {};
 
 loadJSON("config/switcher.json", function(data){
     var decoded = JSON.parse(data);
     setupSwitcher(decoded.styles);
 });
 
-function setupSwitcher(styles){
+function randIntRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function setupSwitcher(categories){
     
     var removeStylePair = function(aPair) {
         Array.prototype.forEach.call(document.querySelectorAll(aPair[0]), function($el) {
@@ -19,14 +23,18 @@ function setupSwitcher(styles){
         });
     }
     
-    var nextStyle = function() {
-        var previousStyleIndex = currentStyleIndex; //-1 means none
-        currentStyleIndex = (currentStyleIndex + 1) % styles.length;
-        if (previousStyleIndex > -1) {
-            styles[previousStyleIndex].forEach(removeStylePair);
+    var pickStyle = function() {
+        for (var style in categories) {
+                var styleCategory = categories[style];
+                if(style in prevStyles){
+                    var oldRandom = prevStyles[style];
+                    styleCategory[oldRandom].forEach(removeStylePair);
+                }
+                var randInt = randIntRange(0,styleCategory.length-1);
+                prevStyles[style] = randInt;
+                styleCategory[randInt].forEach(applyStylePair);
         }
-        styles[currentStyleIndex].forEach(applyStylePair);
     }
     
-    document.body.addEventListener('click', nextStyle);
+    document.body.addEventListener('click', pickStyle);
 }
